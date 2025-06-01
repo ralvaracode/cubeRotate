49438a4bc0fe06f1f370daef736dd749c33f30fe
Ruben Alvarado <ralvaracode@gmail.com>
2025-06-01 12:29:29 -0500

    modified:   README.md 	modified:   main.js 	modified:   three.js

--- README.md
+++ README.md
@@ -6,4 +6,10 @@ It didnt render folling the method suggested in tutorial. I needed to make a loc
 # Reference Video
 https://www.youtube.com/watch?v=oAJNYQLexIQ&list=PLT5klp7W4r8TprwP0xZvVC64WSfRUQFY-
 
-* TODO tinker with options in the scene
+add this to the top of the js files to have documentation of three js functions on hover over
+```
+// @ts-check
+/** @type {import('three')} */
+import * as THREE from 'three';
+```
+you have also the alternative of using typescript
--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 let scene, camera, renderer, cube;
 
 function init() {
-    
+
     // Initialize scene
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0x000000);
 
     // Initialize camera
-    camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 100);
     camera.position.z = 5;
 
     // Initialize renderer
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
-    
+
     // Add renderer to body
     document.body.appendChild(renderer.domElement);
 
@@ -22,12 +22,12 @@ function init() {
     //const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 
     const textureLoader = new THREE.TextureLoader();
-    const texture = textureLoader.load('textures/wall.jpg');    
+    const texture = textureLoader.load('textures/wall.jpg');
     const material = new THREE.MeshBasicMaterial({ map: texture });
     cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 }
-    // Animation loop
+// Animation loop
 function animate() {
     requestAnimationFrame(animate);
     cube.rotation.x += 0.01;
@@ -36,6 +36,8 @@ function animate() {
 }
 
 // window resize
+// this function is used to keep 
+// the cube centered if a windows resize is done
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();

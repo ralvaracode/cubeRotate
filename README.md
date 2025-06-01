a7bb6d9447877214feb98c27ac132f51d1073001
Ruben Alvarado <ralvaracode@gmail.com>
2025-05-31 21:46:05 -0500

    first  commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Implementation of a cube that rotates.
+
+# Trouble Shooting
+It didnt render folling the method suggested in tutorial. I needed to make a local download of three.js to run the scene
+
+# Reference Video
+https://www.youtube.com/watch?v=oAJNYQLexIQ&list=PLT5klp7W4r8TprwP0xZvVC64WSfRUQFY-
---
+++ main.js
@@ -0,0 +1,47 @@
+let scene, camera, renderer, cube;
+
+function init() {
+    
+    // Initialize scene
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x000000);
+
+    // Initialize camera
+    camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
+
+    // Initialize renderer
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    
+    // Add renderer to body
+    document.body.appendChild(renderer.domElement);
+
+    // Create cube
+    const geometry = new THREE.BoxGeometry(2, 2, 2);
+    //const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
+    const textureLoader = new THREE.TextureLoader();
+    const texture = textureLoader.load('textures/wall.jpg');    
+    const material = new THREE.MeshBasicMaterial({ map: texture });
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+}
+    // Animation loop
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+
+// window resize
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize);
+init()
+animate();    
